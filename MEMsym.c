b08adbed227656c885f9fd17ad166ecafdc0a8b1
Jesús León Romero Atienza <jesusleonromero233@gmail.com>
2025-11-18 04:40:44 +0100

    Se corrige la explicación. Siguiente paso dividir el trabajo y empezar con el código

--- MEMsym.c
+++ MEMsym.c
@@ -5,53 +5,55 @@
 //------------------------
 
 
-/* --- Comentario para desarrollar todos los conceptos que contiene el ejercicio y lo que hay que hacer en el. ---
 
-  1 · Explicacion del funcionamiento de una cache con nuestros datos:
+/* --- Comentario para desarrollar todos los conceptos que contiene el ejercicio y lo que hay que hacer en él. ---
+
+  1 · Explicación del funcionamiento de una caché con nuestros datos:
   
 	· La CPU necesita direcciones que contiene la RAM, la CPU puede hacer el proceso de ir a 
-	recoger estas direcciones pero es un porcedimiento que lleva bastante tiempo.
+	recoger estas direcciones, pero es un procedimiento que lleva bastante tiempo.
 	
-    · Por ello, existe nuestra cache que es el intermedieario entre estos dos para optimizar el tiempo de recogida de datos. 
-	Comprueba si lo que ha pedido la CPU almacenado en bloques/lineas lo tiene la RAM o no.  
-    Si lo que ha pedido la CPU esta, es un HIT. Entonces la CPU lee el dato directo de la cache. 
-    Y si no esta, estamos ante un MISS. Y la cache copia el bloque completo de la RAM a la linea correspondiente y actualiza la etiqueta entregando el dato a la CPU. 
+    · Por ello, existe nuestra caché, que es el intermediario entre estos dos para optimizar el tiempo de recogida de datos. 
+	Comprueba si lo que ha pedido la CPU almacenada en bloques/líneas lo tiene la RAM o no.  
+    Si lo que ha pedido la CPU está, es un HIT. Entonces la CPU lee el dato directo de la caché. 
+    Y si no está, estamos ante un MISS. Y la caché copia el bloque completo de la RAM a la línea correspondiente y actualiza la etiqueta entregando el dato a la CPU. 
 
-  2 · Datos e interpretacion de ellos del PDF de la practica:
+
+  2 · Datos e interpretación de ellos del PDF de la práctica:
   
 	Nombre del sistema: MEGATRONIX
 	Cache de correspondencia directa: ETQ / Lin / Pal
 	Dirección de 12 bits: Etiqueta: 5 / Línea: 3 / Palabra: 4
 
-	La cache tiene 8 líneas (2^3), lo definiremos como NUM_FILAS = 8
-	Y utiliza 16 bytes (2^4) por cada linea, se define como TAM_LINEA = 16
+	La caché tiene 8 líneas (2^3), lo definiremos como NUM_FILAS = 8
+	Y utiliza 16 bytes (2^4) por cada línea, se define como TAM_LINEA = 16
 
-	Cuando se inicia la cache esta esta vacia con valores por defecto,
-	por ello la etiqueta tiene un valor por defecto y la linea tambien:
+	Cuando se inicia la caché está esta vacía con valores por defecto,
+	por ello la etiqueta tiene un valor por defecto y la línea también:
 	ETQ_DEFAULT = 0xFF
 	DATOS_DEFAULT = 0x23
 
 	La RAM binaria que tiene una memoria total de 4096 bytes (2^11) 
 	Se debe leer en el archivo "CONTENTS_RAM.bin" que contiene direcciones (en hexadecimal, EJ: A70) que contiene la ram.
-	para la simulacion de esta memoria cache.
+	para la simulación de esta memoria caché.
   
 	El archivo accesos_memoria.txt son las direcciones que la CPU pide a la RAM. 
 
 	Si es un MISS, se incrementa numfallos por 1, se suma 20 a globaltime, se carga el bloque y los mensajes.
-	Si es HIT, se muestra el dato leido.
+	Si es HIT, se muestra el dato leído.
 
-	Finalmnete, tras cada acceso que la CPU hace de la memoria cache se va a:
+	Finalmente, tras cada acceso que la CPU hace de la memoria caché se va a:
 		· Hacer un sleep de 1 segundo, sleep(1).
 		· Mostrar por pantalla los resultados finales: 
 			-Los accesos 	
 			-Los fallos
 			-Tiempo medio
 			-Texto leido.
-			(Formato del PDF: Accesos totales: %d; fallos: %d; Timepo medio: %.2f\nTexto leído: %s)
-		· Escribir/Volcar los 16 bytes de cada linea (8 lineas * 16 bytes = 128 bytes) en el archivo CONTENTS_CACHE.bin 
+			(Formato del PDF: Accesos totales: %d; fallos: %d; Tiempo medio: %.2f\nTexto leído: %s)
+		· Escribir/Volcar los 16 bytes de cada línea (8 líneas * 16 bytes = 128 bytes) en el archivo CONTENTS_CACHE.bin 
 			(byte 0 de ese fichero es el byte 0 de la línea 0 de la caché y el byte 128, es el byte 15 de la línea 15)
   
- -------------------------------------------------------------------------------------------------------------------*/ 
+ -------------------------------------------------------------------------------------------------------------------*/
 
 // 1 · Definicion de librerias, valores y estructuras
   // 1.1 · Librerias
